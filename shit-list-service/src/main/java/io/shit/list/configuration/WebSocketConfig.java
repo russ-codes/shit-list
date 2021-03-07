41fe53cf5192a04f8b12fb94e7a47ca9bcf72b02
Chris <peavers@gmail.com>
2021-03-07 14:09:55 +0000

    fix: Allow websockets to connect when embedded

--- shit-list-service/src/main/java/io/shit/list/configuration/WebSocketConfig.java
+++ shit-list-service/src/main/java/io/shit/list/configuration/WebSocketConfig.java
@@ -14,7 +14,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
 
-    registry.addEndpoint("/socket").setAllowedOrigins("http://localhost:4200").withSockJS();
+    registry
+        .addEndpoint("/socket")
+        .setAllowedOrigins("http://localhost:4200", "http://localhost:8080")
+        .withSockJS();
   }
 
   @Override
