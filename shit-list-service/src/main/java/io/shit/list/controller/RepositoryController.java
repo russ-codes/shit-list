e1c794d68723b62c819e90a7f188c6207fb41253
Chris <peavers@gmail.com>
2021-03-07 14:09:11 +0000

    fix: Process tasks on different threads

--- shit-list-service/src/main/java/io/shit/list/controller/RepositoryController.java
+++ shit-list-service/src/main/java/io/shit/list/controller/RepositoryController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 @CrossOrigin
 @RestController
@@ -16,9 +17,16 @@ public class RepositoryController {
 
   private final RepositoryService repositoryService;
 
+  /**
+   * When saving a new repository, kick off a scan on a different thread so the UI can return as
+   * quick as possible.
+   */
   @PostMapping
   public Mono<Repository> save(@RequestBody final Repository repository) {
-    return repositoryService.save(repository).flatMap(repositoryService::sync);
+    return repositoryService
+        .save(repository)
+        .publishOn(Schedulers.boundedElastic())
+        .doOnSuccess((r) -> repositoryService.sync(r).subscribe());
   }
 
   @GetMapping
@@ -33,9 +41,17 @@ public class RepositoryController {
     return repositoryService.findById(id);
   }
 
+  /**
+   * When deleting a repository, we recalculate all projects. This is a huge waste of resources.
+   * Ideally we will save how many tests/ignores each project has then we can just subtract those
+   * values but that seems like a bit of work...
+   */
   @DeleteMapping("/{id}")
-  public Mono<Void> DeleteById(@PathVariable final long id) {
+  public Mono<Void> deleteById(@PathVariable final long id) {
 
-    return repositoryService.deleteById(id);
+    return repositoryService
+        .deleteById(id)
+        .publishOn(Schedulers.boundedElastic())
+        .doOnSuccess((ignored) -> repositoryService.syncAll().subscribe());
   }
 }
